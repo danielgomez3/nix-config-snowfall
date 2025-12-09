bd99bf20d00d4cc8e5092d830e17d3c94f06e39b
danielgomez3 <danielgomezcoder@gmail.com>
2025-12-09 12:42:04 -0500

    SICK library used! It works

--- modules/nixos/bundles/gui-desktop-environment/default.nix
+++ modules/nixos/bundles/gui-desktop-environment/default.nix
@@ -13,15 +13,16 @@
   ...
 }: let
   cfg = config.profiles.${namespace}.my.nixos.bundles.gui-desktop-environment;
-  inherit (lib) mkEnableOption mkIf;
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) enabled mkBoolOpt;
 in {
   options.profiles.${namespace}.my.nixos.bundles.gui-desktop-environment = {
-    enable = mkEnableOption "Enable custom module for platform 'nixos', of category 'bundles', of module 'gui-desktop-environment', for namespace '${namespace}'.";
+    enable = mkBoolOpt false "Enable custom module for platform 'nixos', of category 'bundles', of module 'gui-desktop-environment', for namespace '${namespace}'.";
   };
   config = mkIf cfg.enable {
     profiles.${namespace}.my.nixos = {
       programs = {
-        gnome.enable = true;
+        gnome = enabled;
       };
     };
   };
--- templates/extra/module/default.nix
+++ templates/extra/module/default.nix
@@ -13,10 +13,12 @@
   ...
 }: let
   cfg = config.profiles.${namespace}.my.xxplatformxx.xxcategoryxx.xxmodulexx;
-  inherit (lib) mkEnableOption mkIf;
+
+  inherit (lib) mkIf;
+  inherit (lib.${namespace}) enabled mkBoolOpt;
 in {
   options.profiles.${namespace}.my.xxplatformxx.xxcategoryxx.xxmodulexx = {
-    enable = mkEnableOption "Enable custom module for platform 'xxplatformxx', of category 'xxcategoryxx', of module 'xxmodulexx', for namespace '${namespace}'.";
+    enable = mkBoolOpt false "Enable custom module for platform 'xxplatformxx', of category 'xxcategoryxx', of module 'xxmodulexx', for namespace '${namespace}'.";
   };
   config =
     mkIf cfg.enable {
